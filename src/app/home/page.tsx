0717b6e7aae91bb344c8bc972729adcfefe274c4
Baijin Chen <chenbj5515@163.com>
2024-09-05 23:51:54 +0900

    feature: vercel db

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
-// import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 // export default async function Home() {
 //   // const memoCards = await res.json();
@@ -31,19 +31,38 @@ import { redirect } from "next/navigation"
 
 export default async function LogoutButton() {
   const session = await auth()
+  const memoCards = await prisma.memo_card.findMany({
+    orderBy: {
+      create_time: 'desc', // 假设使用 'id' 字段来排序
+    },
+    take: 20, // 获取最新的20条数据
+  });
+
+  console.log(memoCards, "memoCards=====")
 
   if (!session) {
     redirect("/api/auth/signin")
   }
 
   return (
-    <form action={async () => {
-      "use server"
-      await signOut({
-        redirectTo: "/"
-      })
-    }}>
-      <button>Sign out</button>
-    </form>
+    <>
+      <div>
+        {memoCards.map((card: any) => (
+          <div key={card.id}>
+            <h2>{card.translation}</h2>
+            <p>Review Times: {card.review_times}</p>
+            {card.kana_pronunciation && <p>Kana: {card.kana_pronunciation}</p>}
+          </div>
+        ))}
+      </div>
+      <form action={async () => {
+        "use server"
+        await signOut({
+          redirectTo: "/"
+        })
+      }}>
+        <button>Sign out</button>
+      </form>
+    </>
   )
 }
