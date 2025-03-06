9ce952d1ccfc23045cdc4a3a2678eb42cc5b1104
Baijin Chen <chenbj5515@163.com>
2025-03-06 21:41:51 +0900

    fix: Terms of UseとPrivacy Policyが逆になった

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -7,8 +7,8 @@ export default function Footer() {
             <div className="flex justify-center items-center space-x-6 text-gray-800">
                 <p className="hover:text-gray-600">Bunn © 2025</p>
                 <span className="text-gray-400">•</span>
-                <a href="/privacy-policy" className="text-gray-400 hover:opacity-90">Terms of Use</a>
-                <a href="/terms-of-service" className="text-gray-400 hover:opacity-90">Privacy Policy</a>
+                <a href="/terms-of-service" className="text-gray-400 hover:opacity-90">Terms of Use</a>
+                <a href="/privacy-policy" className="text-gray-400 hover:opacity-90">Privacy Policy</a>
                 <a href="/business-disclosure" className="text-gray-400 hover:opacity-90">特定商取引法に基づく表示</a>
             </div>
         </footer>
